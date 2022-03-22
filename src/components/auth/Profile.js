c8b3420fdecae2a69983889e5ee35183e1e92829
Anakin Skywalker <jessaydi@gmail.com>
2022-03-22 07:15:26 -0600

    First Update - Login Redirect

---
+++ src/components/auth/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Profile() {
+    return (
+        <section>
+            <div className="container">
+                <h1>Profile!</h1>
+            </div>
+        </section>
+
+    )
+}
---
+++ src/components/auth/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Settings() {
+    return (
+        <section>
+            <div className="container">
+                <h1>Settings!</h1>
+            </div>
+        </section>
+
+    )
+}
